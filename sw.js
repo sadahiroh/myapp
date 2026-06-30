d7576cc3f923e47b44999bb9ad4527bcf3b6efd8
sadahiroh <sadahiro0121@gmail.com>
2026-06-30 19:18:40 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,11 @@
-// Service Worker for オフライン対応
-const CACHE_NAME = 'taxi-record-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
-
+const CACHE = 'taxi-app-v3';
+const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-    ))
-  );
-  self.clients.claim();
+  e.waitUntil(self.clients.claim());
 });
-
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
-  );
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
